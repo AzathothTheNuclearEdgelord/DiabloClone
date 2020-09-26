121cfdf39480185b65c5de0ecf087c778298daff
Azathoth <wandererbetweenplanes@protonmail.com>
2020-09-26 13:01:16 +0200

    Initial commit

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    private float movementX;
+    private float movementZ;
+    public float speed;
+    
+    void Update()
+    {
+        movementX = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed;
+        movementZ = Input.GetAxisRaw("Vertical") * Time.deltaTime * speed;
+        transform.position += new Vector3(movementX, 0, movementZ);
+        /*
+        transform.position += Vector3.forward * movementZ;
+        transform.position += Vector3.right * movementX;
+        */
+        
+    }
+}
